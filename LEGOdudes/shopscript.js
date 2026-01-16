e4328bc17b3a8c333a6307af9065ec7440351484
Daniel Pihlstrøm Karlsen <danielpk@hiof.no>
2026-01-16 13:52:55 +0100

    La til handlevogn funksjoner i backend, og oppdaterende grensesnitt i frontend.

--- LEGOdudes/shopscript.js
+++ LEGOdudes/shopscript.js
@@ -20,13 +20,62 @@ function fetchProducts() {
 
 fetchProducts();
 
+//Generer handleovgn
+function showCart() {
+    //Unike produkter
+    let uniqueItems = new Set(cart);
+    let uniqueArray = [...uniqueItems];
+    //Oversikt over antall per produkt
+    let cartItems = [];
+    //Legger til produkt-id og antall per produkt i cartItems listen
+    uniqueArray.map(item => {
+        cartItems.push({prodid: item, quantity: cart.filter(i => i === item).length});
+    });
+    //Gå gjennom cartItems for å lage HTML til handlekurven og regne ut totalpris
+    let cartHTML = "";
+    let totalPrice = 0;
+    cartItems.map(ci => {
+        //Hente produktinformasjon
+        let product = products.find(i => i.prodid === ci.prodid);
+        cartHTML += `
+            <tr>
+                <td class="title">${product.title}</td>
+                <td class="price">${product.price}</td>
+                <td class="quantity">${ci.quantity}</td>
+                <td class="delete"><button onClick="deleteFromCart(${product.prodid})">X</button></td>
+            </tr>
+        `;
+        //Summere totalpris
+        totalPrice += Number(product.price) * Number(ci.quantity);
+    });
+
+    if(cart.length === 0) {
+        cartHTML += "<tr><td>Ingen varer i handlevognen.</td></tr>"
+    }
+
+    //Oppdatere HTML-elementer
+    document.getElementById("cart-items").innerHTML = cartHTML;
+    document.getElementById("total-price").innerHTML = totalPrice;
+    document.getElementById("cart-quantity").innerHTML = cart.length;
+}
+
+//Slette fra handlevogn
+function deleteFromCart(prodid) {
+    let deleteIndex = cart.indexOf(prodid);
+    if (deleteIndex > -1) {
+        cart.splice(deleteIndex, 1);
+    }
+    //Oppdatere handlevogn-utskrift
+    showCart();
+}
+
 //Legg til i handlevogn:
 function addToCart(prodid) {
     console.log("Legg til prdukt med id: " + prodid);
     cart.push(prodid);
     console.log(cart);
-    document.getElementById("cart-quantity").innerHTML = cart.length;
+    //Oppdatere handlevogn-utskrift
+    showCart();
 }
-
 
 
